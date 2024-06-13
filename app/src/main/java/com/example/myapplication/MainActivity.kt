d9cf4e26317ff09ae0584561716b62a12b5369db
aorus22 <alyzanadif04@gmail.com>
2024-06-13 11:30:44 +0700

    jadi graph dummy

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,7 @@ package com.example.myapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
 import android.widget.ImageView
-import android.widget.LinearLayout
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.GridLayout
